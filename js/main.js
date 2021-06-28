abedd726bb6441468385442afcbfdb91153c5905
nils0n <nilson_jose.v@hotmail.com>
2021-06-28 20:37:55 -0300

    scrollReveal

---
+++ js/main.js
@@ -0,0 +1,12 @@
+const scrollReveal = ScrollReveal({
+  origin: 'top',
+  distance:'30px',
+  duration: 900,
+ reset:true
+});
+
+scrollReveal.reveal(`
+  #about h2, #about p ,
+  .membros .item, .membros .txtMembros , .membros .moto,
+  .encontros .descricao , .membors .foto
+`)
