097ead32b3e04256bc81ac462d93595965b27540
nils0n <nilson_jose.v@hotmail.com>
2021-06-28 21:32:59 -0300

    addInterval

--- js/main.js
+++ js/main.js
@@ -1,12 +1,15 @@
 const scrollReveal = ScrollReveal({
   origin: 'top',
-  distance:'30px',
-  duration: 900,
- reset:true
-});
+  distance: '30px',
+  duration: 700,
+  reset: true
+})
 
-scrollReveal.reveal(`
+scrollReveal.reveal(
+  `
   #about h2, #about p ,
   .membros .item, .membros h3, .membros p , .membros .moto,
   .encontros .descricao , .membors .foto
-`)
+`,
+  { interval: 100 }
+)
